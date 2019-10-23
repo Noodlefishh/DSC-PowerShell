3a48c06240454782c3e1ef236d777b516e7ce92f
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-10-23 15:00:21 -0400

    Create NewDomainController.ps1
    First commit

---
+++ NewDomainController.ps1
@@ -0,0 +1,104 @@
+#Jonathan Bourbonnais - 2019
+#This DSC recipe add a domain controler to an existing forest
+#It do not create a new forest.
+
+#Declare the hostname for the new DC
+$server = Read-Host -Prompt "Entrer le nom du DC a configurer "
+
+#NewDomainController is a function you will be able to call
+Configuration NewDomainController
+{
+    #DECLARE PARAMETER
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $DomainAdministratorCredential
+    )
+
+
+    #Importe DSC ressources
+    #You may need to install them manualy
+    Import-DscResource -ModuleName xActiveDirectory
+    Import-DscResource -ModuleName xNetworking
+    Import-DscResource -ModuleName NetworkingDsc     
+
+    
+    node $server
+    {
+        
+        #Disable DHCP on the network adapter
+        NetIPInterface 'DisableDHCP' 
+        {    
+            InterfaceAlias = 'Ethernet_lan'
+            AddressFamily  = 'IPV4'
+            Dhcp           = 'Disabled'
+        }
+
+        #Configure static IP adress
+        IPAddress 'NewIPV4Address' 
+        {
+            IPAddress      = '10.40.38.110'
+            InterfaceAlias = 'Ethernet_lan'
+            AddressFamily  = 'IPV4'
+            DependsOn = '[NetIPInterface]DisableDHCP'
+
+        }
+        
+        #Install ADDS
+        WindowsFeature 'InstallADDomainServicesFeature'
+        {
+            Ensure = 'Present'
+            Name   = 'AD-Domain-Services'
+        }
+
+        #Install PowerShell RSAT
+        WindowsFeature 'RSATADPowerShell'
+        {
+            Ensure    = 'Present'
+            Name      = 'RSAT-AD-PowerShell'
+            DependsOn = '[WindowsFeature]InstallADDomainServicesFeature'
+        }
+
+        #Install RSAT GUI
+        WindowsFeature 'RSATADDSTools'
+        {
+            Ensure     = 'Present'
+            Name       = 'RSAT-ADDS'
+            DependsOn  = '[WindowsFeature]RSATADPowershell'
+        }
+
+        #Check if the domain exist
+        xWaitForADDomain 'WaitForestAvailability'
+        {
+            
+            DomainName           = 'capitalelab.local'
+            DomainUserCredential = $DomainAdministratorCredential
+            RetryCount           = 10
+            RetryIntervalSec     = 120
+            DependsOn            = '[WindowsFeature]RSATADDSTools'
+        }
+
+        #Configure ADDS
+        xADDomainController 'DomainControllerAllProperties'
+        {
+            DomainName                     = 'contoso.com'
+            DomainAdministratorCredential  = $DomainAdministratorCredential
+            SafemodeAdministratorPassword  = $DomainAdministratorCredential
+            DatabasePath                   = 'C:\Windows\NTDS'
+            LogPath                        = 'C:\Windows\Logs'
+            SysvolPath                     = 'C:\Windows\SYSVOL'
+            #SiteName                      = ''
+            #isGlobalCatalog               = $true 
+            DependsOn                      = '[xWaitForADDomain]WaitForestAvailability'
+        }
+
+    }
+}
+
+#Start the DSC Configuration
+Start-DscConfiguration -Path C:\Path\To\DSC\Config\File\NewDomainController\ -ComputerName "YOUR-PC-NAME" -Verbose -Debug -Wait -Force 
+
+#Run this if you need to install manualy a module
+#Find-Module -Name NetworkingDsc -Repository PSGallery | Install-Module
