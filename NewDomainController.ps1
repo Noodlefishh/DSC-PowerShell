430165f465beff617cd3c690c30d95f47cfbdcf3
Jonathan Bourbonnais <49311040+jonathanbourbonnais@users.noreply.github.com>
2019-10-24 12:37:38 -0400

    Update NewDomainController.ps1
    Add description

--- NewDomainController.ps1
+++ NewDomainController.ps1
@@ -1,7 +1,6 @@
 #Jonathan Bourbonnais - 2019
-#This DSC recipe add a domain controller to an existing forest
+#This DSC recipe add a domain controller to an existing forest (it do not create a new forest)
 #This script is provided as-is and is configured in push mode
-#It do not create a new forest.
 #For further direction on DSC, refer to : https://docs.microsoft.com/en-us/powershell/scripting/dsc/resources/resources?view=powershell-6
 
 #Declare the hostname for the new DC
